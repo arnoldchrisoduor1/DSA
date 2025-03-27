d42e21bdea0afb0575cf90d55b043d6439676642
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-03-27 21:47:47 +0300

    no changes at all ha ha ha

--- 006-Get-Middle-Character/main.cpp
+++ 006-Get-Middle-Character/main.cpp
@@ -9,8 +9,10 @@ string get_laptop(string s)  {
 
     // for even number of characters in the string.
     if (length % 2 == 0 ) {
+
         int value = length / 2;
         result = string(1, s[value - 1]) + s[value];
+
     } else {
         int value = length / 2;
         result = string(1, s[value]);
@@ -20,6 +22,7 @@ string get_laptop(string s)  {
 
 int main() {
     cout << "Current state: " << get_laptop("textin") << endl;
+    cout << "Current state: " << get_laptop("hello") << endl;
     cout << "Current state: " << get_laptop("hello") << endl;
     return 0;
 }
